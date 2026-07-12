44c7d99a209d9eccf8d8ce3ddc2e24806a8521f4
Facely Conde <facelycond3@gmail.com>
2026-07-12 20:31:37 +0200

    Finalisation de la page Mes commandes

--- patients/urls.py
+++ patients/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("inscription/", views.inscription, name="inscription"),
     path("connexion/", views.connexion, name="connexion"),
     path("deconnexion/", views.deconnexion, name="deconnexion"),
+    path("mes-commandes/", views.mes_commandes, name="mes_commandes"),
 ]
