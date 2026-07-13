314b5905f93877f084c2bc75a42d993e4baa8ed4
Facely Conde <facelycond3@gmail.com>
2026-07-13 22:30:42 +0200

    Ajouter l'import Excel des pharmacies

--- pharmacies/admin.py
+++ pharmacies/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Pharmacie, Stock
 
 
+class PharmacieResource(resources.ModelResource):
+    # colonnes attendues : nom, quartier, adresse, latitude, longitude,
+    # numero_agrement_onpg, telephone, est_de_garde, est_verifiee.
+    # quartier = un des codes exacts (kaloum/ratoma/matoto/dixinn/matam), pas le
+    # nom affiché. import_id_fields vide -> toujours des NOUVELLES fiches.
+    class Meta:
+        model = Pharmacie
+        fields = (
+            "nom", "quartier", "adresse", "latitude", "longitude",
+            "numero_agrement_onpg", "telephone", "est_de_garde", "est_verifiee",
+        )
+        import_id_fields = ()
+
+
 @admin.register(Pharmacie)
-class PharmacieAdmin(admin.ModelAdmin):
+class PharmacieAdmin(ImportExportModelAdmin):
+    resource_classes = [PharmacieResource]
     list_display = ("nom", "quartier", "telephone", "est_de_garde", "est_verifiee", "compte_gestionnaire")
     search_fields = ("nom", "numero_agrement_onpg")
     list_filter = ("quartier", "est_de_garde", "est_verifiee")
